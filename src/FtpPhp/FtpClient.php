66fbc0c74a5f943c2164b37011d85c05401067b1
Reinier Kip <github@reinierkip.nl>
2012-07-27 11:22:17 +0200

    Making strings of FtpClient constants.

--- src/FtpPhp/FtpClient.php
+++ src/FtpPhp/FtpClient.php
@@ -14,16 +14,16 @@ namespace FtpPhp;
 class Ftp
 {
 	/**#@+ FTP constant alias */
-	const ASCII = FTP_ASCII;
-	const TEXT = FTP_TEXT;
-	const BINARY = FTP_BINARY;
-	const IMAGE = FTP_IMAGE;
-	const TIMEOUT_SEC = FTP_TIMEOUT_SEC;
-	const AUTOSEEK = FTP_AUTOSEEK;
-	const AUTORESUME = FTP_AUTORESUME;
-	const FAILED = FTP_FAILED;
-	const FINISHED = FTP_FINISHED;
-	const MOREDATA = FTP_MOREDATA;
+	const ASCII = "FTP_ASCII";
+	const TEXT = "FTP_TEXT";
+	const BINARY = "FTP_BINARY";
+	const IMAGE = "FTP_IMAGE";
+	const TIMEOUT_SEC = "FTP_TIMEOUT_SEC";
+	const AUTOSEEK = "FTP_AUTOSEEK";
+	const AUTORESUME = "FTP_AUTORESUME";
+	const FAILED = "FTP_FAILED";
+	const FINISHED = "FTP_FINISHED";
+	const MOREDATA = "FTP_MOREDATA";
 	/**#@-*/
 
 	private static $aliases = array(
