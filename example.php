da6ed2b6a186e520f9a8c6060af70387b872f67a
Reinier Kip <github@reinierkip.nl>
2012-07-27 11:21:37 +0200

    Updating example code.

--- example.php
+++ example.php
@@ -1,26 +1,28 @@
 <?php
 
-require_once 'ftp.class.php';
+require_once "vendor/autoload.php";
 
+use FtpPhp\FtpClient;
+use FtpPhp\FtpException;
 
 try {
-	$ftp = new Ftp;
+	$ftp = new FtpClient;
 
 	// Opens an FTP connection to the specified host
-	$ftp->connect('ftp.ed.ac.uk');
+	$ftp->connect("ftp.ed.ac.uk");
 
 	// Login with username and password
-	$ftp->login('anonymous', 'example@example.com');
+	$ftp->login("anonymous", "example@example.com");
 
-	// Download file 'README' to local temporary file
+	// Download file "README" to local temporary file
 	$temp = tmpfile();
-	$ftp->fget($temp, 'README', Ftp::ASCII);
+	$ftp->fget($temp, "README", FtpClient::ASCII);
 
 	// echo file
-	echo '<pre>';
+	echo "<pre>";
 	fseek($temp, 0);
 	fpassthru($temp);
 
 } catch (FtpException $e) {
-	echo 'Error: ', $e->getMessage();
+	echo "Error: ", $e->getMessage();
 }
